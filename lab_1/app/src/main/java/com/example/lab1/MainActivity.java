cbf6d40f7b226a3fe0dc9f96fb2ff2ea69245487
Denis Petrovsky <vertuss111@gmail.com>
2019-10-03 23:50:28 +0300

    Fix bug with first lab

---
+++ lab_1/app/src/main/java/com/example/lab1/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onMyClick(View view) {
+        // process button click
+        TextView textView = (TextView) findViewById(R.id.textView);
+        textView.setText("Hello world!");
+    }
+}
