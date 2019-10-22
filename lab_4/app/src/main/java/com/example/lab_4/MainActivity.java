22fb370940afa198b8237bd323cedb36ba12bebe
Denis Petrovsky <vertuss111@gmail.com>
2019-10-22 19:43:00 +0300

    lab6 all

--- lab_4/app/src/main/java/com/example/lab_4/MainActivity.java
+++ lab_4/app/src/main/java/com/example/lab_4/MainActivity.java
@@ -1,14 +1,66 @@
 package com.example.lab_4;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity {
+
+    private ObjectAnimator animation1;
+    private ObjectAnimator animation2;
+    private Button button;
+    private Random randon;
+    private int width;
+    private int height;
+    private AnimatorSet set;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.doctor_chemist_list);
+        width = getWindowManager().getDefaultDisplay().getWidth();
+        height = getWindowManager().getDefaultDisplay().getHeight();
+        randon = new Random();
+        set = createAnimation();
+        set.start();
+        set.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                int nextX = randon.nextInt(width);
+                int nextY = randon.nextInt(height);
+                animation1 = ObjectAnimator.ofFloat(button, "x", button.getX(),
+                        nextX);
+                animation1.setDuration(1400);
+                animation2 = ObjectAnimator.ofFloat(button, "y", button.getY(),
+                        nextY);
+                animation2.setDuration(1400);
+                set.playTogether(animation1, animation2);
+                set.start();
+            }
+        });
+    }
+
+    public void onClick(View view) {
+        String string = button.getText().toString();
+        int hitTarget = Integer.valueOf(string) + 1;
+        button.setText(String.valueOf(hitTarget));
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    @SuppressLint("NewApi")
+
+    private AnimatorSet createAnimation() {
+        int nextX = randon.nextInt(width);
+        int nextY = randon.nextInt(height);
+        button = (Button) findViewById(R.id.Button01);
+        animation1 = ObjectAnimator.ofFloat(button, "x", nextX);
+        animation1.setDuration(1400);
+        animation2 = ObjectAnimator.ofFloat(button, "y", nextY);
+        animation2.setDuration(1400);
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(animation1, animation2);
+        return set;
     }
 }
--- lab_6/app/src/main/java/com/example/lab_6/MainActivity.java
+++ lab_6/app/src/main/java/com/example/lab_6/MainActivity.java
@@ -1,13 +1,72 @@
-package com.example.lab_6;
+package com.example.lab_5;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.lab_6.FileOperations;
+import com.example.lab_6.R;
+
+import java.io.IOException;
+
+public class MainActivity extends Activity {
+
+    EditText fname,fcontent,fnameread;
+    Button write,read;
+    TextView filecon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fname = (EditText) findViewById(R.id.fname);
+        fcontent = (EditText) findViewById(R.id.ftext);
+        fnameread = (EditText) findViewById(R.id.fnameread);
+        write = (Button) findViewById(R.id.btnwrite);
+        read = (Button) findViewById(R.id.btnread);
+        filecon = (TextView) findViewById(R.id.filecon);
+        write.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                String filename = fname.getText().toString();
+                String filecontent = fcontent.getText().toString();
+                FileOperations fop = new FileOperations();
+                fop.write(filename, filecontent);
+                if(fop.write(filename, filecontent)){
+                    Toast.makeText(getApplicationContext(),
+                            filename+".txt created", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(getApplicationContext(), "I/O error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        read.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                String readfilename = fnameread.getText().toString();
+                FileOperations fop = new FileOperations();
+                String text = fop.read(readfilename);
+                if(text != null){
+                    filecon.setText(text);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "File not Found", Toast.LENGTH_SHORT).show();
+                    filecon.setText(null);
+                }
+            }
+        });
     }
 }
